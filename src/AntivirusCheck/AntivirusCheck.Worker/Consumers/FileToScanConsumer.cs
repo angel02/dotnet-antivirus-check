aaa8054bf07fc9dd8a2802f1dad4e0fd6311dfee
Angel Paulino <apaulino002@gmail.com>
2023-06-04 21:58:30 -0400

    Add consumer logic and configuration

---
+++ src/AntivirusCheck/AntivirusCheck.Worker/Consumers/FileToScanConsumer.cs
@@ -0,0 +1,52 @@
+﻿using AntivirusCheck.Shared.Contracts;
+using MassTransit;
+using nClam;
+
+namespace AntivirusCheck.Worker.Consumers
+{
+    public class FileToScanConsumer : IConsumer<FileToScan>
+    {
+        private readonly ILogger<FileToScanConsumer> logger;
+
+        public FileToScanConsumer(ILogger<FileToScanConsumer> logger)
+        {
+            this.logger = logger;
+        }
+
+        public async Task Consume(ConsumeContext<FileToScan> context)
+        {
+            var fileToScanData = context.Message;
+            var address = Environment.GetEnvironmentVariable("ClamAV_Address");
+            var port = int.Parse(Environment.GetEnvironmentVariable("ClamAV_Port"));
+
+            var clamav = new ClamClient(address, port);
+            var result = await clamav.SendAndScanFileAsync(fileToScanData.FullPath);
+
+            var fileScanResponse = new FileScanResult
+            {
+                FileName = fileToScanData.FileName,
+                FullPath = fileToScanData.FullPath
+            };
+
+            fileScanResponse.Message = result.RawResult;
+
+            switch (result.Result)
+            {
+                case ClamScanResults.Clean:
+                    logger.LogInformation("File clean");
+                    fileScanResponse.IsClean = true;
+                    break;
+                case ClamScanResults.VirusDetected:
+                    logger.LogWarning("Malware detected !!");
+                    fileScanResponse.IsClean = false;
+                    break;
+                case ClamScanResults.Error:
+                    logger.LogError("There was an error while scanning the file");
+                    break;
+                default:
+                    logger.LogInformation("Something else happened");
+                    break;
+            }
+        }
+    }
+}
--- src/AntivirusCheck/AntivirusCheck.Worker/Program.cs
+++ src/AntivirusCheck/AntivirusCheck.Worker/Program.cs
@@ -1,9 +1,27 @@
-using AntivirusCheck.Worker;
+using AntivirusCheck.Worker.Consumers;
+using MassTransit;
 
 IHost host = Host.CreateDefaultBuilder(args)
     .ConfigureServices(services =>
     {
-        services.AddHostedService<Worker>();
+        //services.AddHostedService<Worker>();
+        services.AddMassTransit(config =>
+        {
+            config.AddConsumer<FileToScanConsumer>();
+            config.UsingRabbitMq((context, rabbitConfig) =>
+            {
+                var address = Environment.GetEnvironmentVariable("RabbitMQ_Address");
+
+                rabbitConfig.Host(new Uri(address), "TestConnection", host => {
+                    host.Username(Environment.GetEnvironmentVariable("RabbitMQ_User"));
+                    host.Password(Environment.GetEnvironmentVariable("RabbitMQ_Pass"));
+                });
+
+                rabbitConfig.ConfigureEndpoints(context);
+            });
+        });
+
+
     })
     .Build();
 
