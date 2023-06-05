2c2315bb49dfe48195d8b4ba6fdc93da4a940e73
Angel Paulino <apaulino002@gmail.com>
2023-06-04 21:57:02 -0400

    Add upload file logic

--- src/AntivirusCheck/AntivirusCheck.Api/Controllers/FileController.cs
+++ src/AntivirusCheck/AntivirusCheck.Api/Controllers/FileController.cs
@@ -1,3 +1,5 @@
+using AntivirusCheck.Shared.Contracts;
+using MassTransit;
 using Microsoft.AspNetCore.Mvc;
 
 namespace AntivirusCheck.Api.Controllers
@@ -6,16 +8,32 @@ namespace AntivirusCheck.Api.Controllers
     [Route("[controller]")]
     public class FileController : ControllerBase
     {
-        private readonly ILogger<FileController> _logger;
+        private readonly ILogger<FileController> logger;
+        private readonly IPublishEndpoint publishEndpoint;
 
-        public FileController(ILogger<FileController> logger)
+        public FileController(ILogger<FileController> logger, IPublishEndpoint publishEndpoint)
         {
-            _logger = logger;
+            this.logger = logger;
+            this.publishEndpoint = publishEndpoint;
         }
 
+
         [HttpPost(Name = "UploadFile")]
-        public string Post(IFormFile file)
+        public async Task<string> Post(IFormFile file)
         {
+            var filesPath = Environment.GetEnvironmentVariable("FilePath");
+            var fileName = Guid.NewGuid().ToString();
+            var fullPath = Path.Combine(filesPath, fileName);
+
+            using var fileStream = new FileStream(fullPath, FileMode.OpenOrCreate, FileAccess.Write);
+            await file.CopyToAsync(fileStream);
+
+            await this.publishEndpoint.Publish(new FileToScan
+            {
+                FullPath = fullPath,
+                FileName = fileName
+            });
+
             return "Uploaded";
         }
     }
--- src/AntivirusCheck/AntivirusCheck.Api/Program.cs
+++ src/AntivirusCheck/AntivirusCheck.Api/Program.cs
@@ -1,3 +1,5 @@
+using MassTransit;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +8,18 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddMassTransit(config =>
+{
+    config.UsingRabbitMq((context, config) =>
+    {
+        var address = Environment.GetEnvironmentVariable("RabbitMQ_Address");
+
+        config.Host(new Uri(address), "TestConnection", host => {
+            host.Username(Environment.GetEnvironmentVariable("RabbitMQ_User"));
+            host.Password(Environment.GetEnvironmentVariable("RabbitMQ_Pass"));
+        });
+    });
+});
 
 var app = builder.Build();
 
