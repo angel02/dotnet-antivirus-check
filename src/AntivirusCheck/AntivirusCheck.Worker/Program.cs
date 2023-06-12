2e28a5129fda9a27659615e56fea9c760c776e59
Angel Paulino <apaulino002@gmail.com>
2023-06-11 21:45:57 -0400

    Remove unnecessary code

--- src/AntivirusCheck/AntivirusCheck.Worker/Program.cs
+++ src/AntivirusCheck/AntivirusCheck.Worker/Program.cs
@@ -4,7 +4,6 @@ using MassTransit;
 IHost host = Host.CreateDefaultBuilder(args)
     .ConfigureServices(services =>
     {
-        //services.AddHostedService<Worker>();
         services.AddMassTransit(config =>
         {
             config.AddConsumer<FileToScanConsumer>();
